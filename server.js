56dc9f82483478accb600b65e245038ebf1db61e
SheryarAmir <dev.sheryaramir@gmail.com>
2026-01-26 11:29:34 +0500

    create a server to check

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Route 1: Health Check
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running fine"
+  });
+});
+
+
+//  Route 2: Home
+ 
+app.get("/", (req, res) => {
+  res.send("Welcome to my Node.js server on AWS EC2");
+});
+
+// Route 3: Data Endpoint
+app.post("/data", (req, res) => {
+  const body = req.body;
+
+  res.json({
+    success: true,
+    receivedData: body
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
